fb39fc74831317fd9afd3955bbc50a91fb48b0ea
DanishSparky <danishsparkydt@gmail.com>
2025-12-02 21:21:00 +0530

    Details page Categories Added

--- details.js
+++ details.js
@@ -1,3 +1,4 @@
+let mealProducts = []
 const clicked_ID = localStorage.getItem("clickedID");
 
 async function fetchMealDetail(mealID) {
@@ -11,4 +12,47 @@ fetchMealDetail(clicked_ID)
 
 function displaymealDetails(details){
 
+}
+
+
+
+
+
+async function fetchItems(){
+    const response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    const data = await response.json()
+    data.categories.forEach((obj) => {
+        mealProducts.push(obj)
+        
+    })
+    displayItems(data.categories)
+}
+fetchItems()
+
+function displayItems(meals) {
+    const category = document.getElementById("mealsRow");
+    let meal = "";
+
+    meals.forEach((product) => {
+        meal += `
+            <div class="measlCategories" data-category="${product.strCategory}" data-description="${product.strCategoryDescription}">
+                <h5 id="categoryTitle">${product.strCategory}</h5>
+                <img src="${product.strCategoryThumb}" alt="${product.strCategory}">
+            </div>
+        `;
+    });
+
+    category.innerHTML = meal;
+    document.querySelectorAll(".measlCategories").forEach((card) => {
+        card.addEventListener("click", () =>{
+            const categoryName = card.getAttribute("data-category");
+            const description = card.getAttribute("data-description");
+            
+            // save category and description in localstorage
+            localStorage.setItem("selectedCategory", categoryName);
+            localStorage.setItem("selectedDescription", description);
+
+            window.location.href = "meals.html";
+        })
+    })
 }
