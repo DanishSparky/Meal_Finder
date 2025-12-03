93e5d6011a1ed96597f2ca8dae6ae30c8e0d4688
DanishSparky <danishsparkydt@gmail.com>
2025-12-04 03:52:08 +0530

    Meals details page done

--- details.js
+++ details.js
@@ -10,11 +10,11 @@ async function fetchMealDetail(mealID) {
 
 fetchMealDetail(clicked_ID)
 
-function displaymealDetails(details){
+function displaymealDetails(details) {
     const detail = document.getElementById("secondNav");
     let anotherNav = "";
 
-    details.forEach((meal)=>{
+    details.forEach((meal) => {
         anotherNav += `
         <div class="anotherNav">
             <a href="./index.html"><i class="fa-solid fa-house" id="icon"></i></a>
@@ -23,60 +23,120 @@ function displaymealDetails(details){
         </div>
         `
     })
-    detail.innerHTML = anotherNav; 
+    detail.innerHTML = anotherNav;
 
 
 
     const info = document.getElementById("mealDetail");
     let mainDetail = "";
 
-    details.forEach((information)=>{
+    details.forEach((information) => {
 
-         let ingredientsList = "";
-
-    for(let i = 1; i <= 20; i++){
-        const ingredient = information[`strIngredient${i}`];
-        if (ingredient && ingredient.trim() !== "") {
-            ingredientsList += `<p>${i} ${ingredient}</p>`;
+        let ingredientsList = "";
+        for (let i = 1; i <= 20; i++) {
+            const ingredient = information[`strIngredient${i}`];
+            if (ingredient && ingredient.trim() !== "") {
+                ingredientsList += `<p><span>${i}</span> ${ingredient}</p>`;
+            }
         }
-    }
+
+
+        let tagsList = "";
+
+        if (information.strTags) {
+            const tags = information.strTags.split(",");
+
+            tags.forEach(tag => {
+                if (tag.trim() !== "") {
+                    tagsList += `<span class="singleTag">${tag.trim()}</span>`;
+                }
+            });
+        }
+
+
+        let measuresList = "";
+        for (let i = 1; i <= 20; i++) {
+            const measure = information[`strMeasure${i}`];
+
+            if (measure && measure.trim() !== "") {
+                measuresList += `
+            <p>
+                <i class="fa-solid fa-spoon" style="color: #e16120; margin-right: 6px;"></i>
+                ${measure.trim()}
+            </p>
+        `;
+            }
+        }
+
+
+        let instructionsList = "";
+        let steps = information.strInstructions
+            .split(".")
+            .map(step => step.trim())
+            .filter(step => step !== "");
+
+        steps.forEach(step => {
+            instructionsList += `
+        <p>
+            <i class="fa-regular fa-square-check" style="color: #e16120; margin-right: 8px;"></i>
+            ${step}.
+        </p>
+        <br>
+    `;
+        });
+
+        let mealCategory = information.strCategory
 
         mainDetail += `
-        <div class="imgdiv">
+        <div class="mealParameters">
             <img src="${information.strMealThumb}" alt="${information.strMeal}">
-        </div>
-        <div class="mealInfo">
-            <h2 class="mealName">${information.strMeal}</h2>
-            <hr class="belowHr">
-            <p class="mealCategory"> CATEGORY : ${information.strCategory}</p>
-            <p class="mealSource"> Source : ${information.strSource}</p>
-            <span class="mealTags"> Tags : ${information.strTags}</span>
-            <div class="mealIngridents">
-                <p class="ingredientsTitle">Ingredients</p>
-                <div class="allIngredients">
-                    ${ingredientsList}
+            <div class="mealInfo">
+                <h2 class="mealName">${information.strMeal}</h2>
+                <hr>
+                <p class="mealCategory"> CATEGORY: <span> ${mealCategory.toUpperCase()}</span></p>
+                <p class="mealSource"> Source: <span> ${information.strSource}</span></p>
+                <div class="mealTags">
+                    <p>Tags :</p>
+                    ${tagsList}
+                </div>
+                <div class="mealIngridents">
+                    <h5 class="ingredientsTitle">Ingredients</h5>
+                    <div class="allIngredients">
+                        ${ingredientsList}
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div class="measures">
+            <h5 class="measureTitle">Measure:</h5>
+            <div class="allMeasures">
+                ${measuresList}
+            </div>
+        </div>
+
+        <div class="instrunctions">
+             <p class="instructionsTitle">Instructions:</p>
+            <div class="allInstructions">
+                ${instructionsList}
+            </div>
+        </div>
+
         `
     })
     info.innerHTML = mainDetail;
-
-    
-    
-
 }
 
 
 
 
 
-async function fetchItems(){
+async function fetchItems() {
     const response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     const data = await response.json()
     data.categories.forEach((obj) => {
         mealProducts.push(obj)
-        
+
     })
     displayItems(data.categories)
 }
@@ -97,10 +157,10 @@ function displayItems(meals) {
 
     category.innerHTML = meal;
     document.querySelectorAll(".measlCategories").forEach((card) => {
-        card.addEventListener("click", () =>{
+        card.addEventListener("click", () => {
             const categoryName = card.getAttribute("data-category");
             const description = card.getAttribute("data-description");
-            
+
             // save category and description in localstorage
             localStorage.setItem("selectedCategory", categoryName);
             localStorage.setItem("selectedDescription", description);
